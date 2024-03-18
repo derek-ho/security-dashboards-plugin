b940ad04a183ace04c21a92de2e2b4f4221c67b4
Derek Ho <dxho@amazon.com>
2024-03-18 16:43:54 -0400

    Try
    Signed-off-by: Derek Ho <dxho@amazon.com>

--- test/cypress/support/commands.js
+++ test/cypress/support/commands.js
@@ -91,7 +91,7 @@ Cypress.Commands.add('loginWithSamlMultiauth', () => {
   cy.get('button[id=btn-sign-in]').should('be.visible').click();
 });
 
-Cypress.Commands.overwrite('visit', (orig, url, options = {}) => {
+Cypress.Commands.overwrite('visit', (orig, url, options) => {
   if (Cypress.env('LOGIN_AS_ADMIN')) {
     options.auth = ADMIN_AUTH;
   }
